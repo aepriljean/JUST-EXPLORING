e637979fc0e7ab7d3b153f9e0342d09a3aa334ff
Aepril <aepa_binwag@nvsu.edu.ph>
2022-11-29 10:01:35 +0700

    commit initial just exploring

---
+++ R4everyone.R
@@ -0,0 +1,91 @@
+1+1 # addition
+
+44+5+6
+
+3*7*2 # MULTIPLICATION
+
+4+(40+2)/8 # when doing arithmetic, PEMDAS
+
+x <- 3 # variables
+
+x = 4
+
+x <- y <- 5
+
+boy <- 67 
+
+age_boys <- 17
+
+rm(age_boys) # removing variables
+
+rm(x,y) # removing a list
+
+x <- 7L
+
+is.numeric(x) # on numeric data
+
+is.integer(x)
+
+class(x)
+
+y <- "love"
+
+assign("z", "love") # assigning variables
+
+z <- c("CL", "MC", "SC") # FACTOR
+
+z <- factor(z,levels = c("CL", "MC", "SC"))
+
+y <- c(2, 5, 7)
+
+k <- c(TRUE, FALSE, TRUE, TRUE) # logical
+
+j <- c(T, F, T, T, F, F)
+
+g <- c("M", "F")
+
+h <- c(j,k)
+
+5 !=10/2
+
+2<3
+2<=3
+
+"aua" < "ata" # must be alphabetically ordered
+
+x <- 1:20 #assigning x-values from 1-20
+
+x
+1:20
+
+x <- 1:10 # vectors
+
+y <- 2:6
+
+x * y
+
+length(x)
+length(y)
+
+x <- 1:10
+x < 5
+
+rm(q,r)
+
+s <- c(1,2,3,4,5,6,7,8,9,10) #VECTOR OPERATIONS
+
+s * 3 #multiplication
+
+s/4 #division
+
+sqrt(s) #square roots
+
+a <- 1:10
+b <- -5:4
+
+a+b
+
+a-b
+
+mean(s) # calling functions
+
